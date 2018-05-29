8ee428d2cdb251d99cc228bab8171b0404d0eb80
Richard Myatt <lydgate77@hotmail.co.uk>
2018-05-29 22:32:36 +0100

    App now working

--- js/leader_board.js
+++ js/leader_board.js
@@ -1,14 +1,34 @@
-var db = [];
-var dataUrl = "https://fcctop100.herokuapp.com/api/fccusers/top/alltime";   // recent
-
-function init() {
-fetch("json/data_recent_small.json")
-.then(response => response.json())
-.then(function(data) {
-db = data;                     // place the database in the global variable db
-console.log(data);
-})
-.catch(error => console.log("There was an error: ", error));
-}
-
-init();
+var main = new Vue({
+  el: "#app",
+  data: {
+    recentEnabled: true,
+    urlRecent: "https://fcctop100.herokuapp.com/api/fccusers/top/recent",
+    urlAlltime: "https://fcctop100.herokuapp.com/api/fccusers/top/alltime",
+    userData: []
+  },
+  methods: {
+    getData: function(url) {
+      fetch(url)
+      .then(response => response.json())
+      .then(function(data) {
+      main.userData = data;
+      })
+      .catch(error => console.log("There was an error: ", error));
+    },
+    getRecent: function() {
+      if(!this.recentEnabled) {
+        this.getData(this.urlRecent);
+        this.recentEnabled = true;
+      }
+    },
+    getAlltime: function() {
+      if(this.recentEnabled) {
+        this.getData(this.urlAlltime);
+        this.recentEnabled = false;
+      }
+    }
+  },
+  created: function() {
+    this.getData(this.urlRecent);
+  }
+});
