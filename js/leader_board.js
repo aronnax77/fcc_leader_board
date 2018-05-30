8639569781b971e667a761deebab78680f9d2eea
Richard Myatt <lydgate77@hotmail.co.uk>
2018-05-30 15:24:18 +0100

    Application now working

--- js/leader_board.js
+++ js/leader_board.js
@@ -1,4 +1,11 @@
-var main = new Vue({
+/*   Author: Richard Myatt
+     Date: 30 May 2018
+
+     An exercise in data visualization and a first use of materialize.
+*/
+
+// main vue.js instance
+new Vue({
   el: "#app",
   data: {
     recentEnabled: true,
@@ -7,20 +14,21 @@ var main = new Vue({
     userData: []
   },
   methods: {
+    // helper method to fetch remote data
     getData: function(url) {
       fetch(url)
       .then(response => response.json())
-      .then(function(data) {
-      main.userData = data;
-      })
+      .then(data => {this.userData = data})
       .catch(error => console.log("There was an error: ", error));
     },
+    // retrieve data relating to recent activity
     getRecent: function() {
       if(!this.recentEnabled) {
         this.getData(this.urlRecent);
         this.recentEnabled = true;
       }
     },
+    // retrieve data relating to all time scores
     getAlltime: function() {
       if(this.recentEnabled) {
         this.getData(this.urlAlltime);
@@ -28,6 +36,7 @@ var main = new Vue({
       }
     }
   },
+  // set up initial view of board by retrieving recent scores data
   created: function() {
     this.getData(this.urlRecent);
   }
